c7b2d02160d16dc987e1db391562792a4c45bb67
unknown <laurent.supplisson@gmail.com>
2024-02-27 09:46:11 +0100

    fixing rsponsive header

--- src/app/donneesdusite/page.tsx
+++ src/app/donneesdusite/page.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
 import Header from "../components/header/page";
 import Footer from "../components/footer/page";
-
 
 const Donneesdusite = () => {
   return (
     <div>
       <Header />
-      <div className="donnéesMainContener" >
-        <h1 className='donnéeTitreMain' >Données du site</h1>
-      <span className="donnéesSpanMain" >Chez bluePoint, on aime : 
-      <ul className='donnéeContenerListe'>
-        <li>- Les Tielles,</li>
-        <li>- Les petits patés de Pézenas,</li>
-        <li>- Les huites de l&apos;étange de Thau</li>
-        <li>- Les grisettes de Montpellier,</li>
-        </ul>   mais pas les cookies. Alors rassurez vous, on ne collecte aucunes données.  </span>
+      <div className="donnéesMainContener">
+        <h1 className="donnéeTitreMain">Données du site</h1>
+        <div className="donnéeContenerTexe" >
+          <span className="donnéesSpanMain">
+            Chez bluePoint, on aime :
+            <ul className="donnéeContenerListe">
+              <li>- Les Tielles,</li>
+              <li>- Les petits patés de Pézenas,</li>
+              <li>- Les huitres de l&apos;étange de Thau</li>
+              <li>- Les grisettes de Montpellier,</li>
+            </ul>{" "}
+            mais pas les cookies. Alors rassurez vous, on ne collecte aucunes
+            données.{" "}
+          </span>
+        </div>
       </div>
       <Footer />
     </div>
@@ -24,5 +29,3 @@ const Donneesdusite = () => {
 };
 
 export default Donneesdusite;
-
-
