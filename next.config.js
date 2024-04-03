815c2ff735a92e8ff9c64297cd228c2f5fa98eab
unknown <laurent.supplisson@gmail.com>
2024-04-03 11:00:27 +0200

    tryingnext-SiteMap

--- next.config.js
+++ next.config.js
@@ -1,25 +1,17 @@
+const withSitemap = require('next-sitemap');
 
-module.exports = async (phase, { defaultConfig }) => {
-  const withSitemap = require('@next/bundle-analyzer-plugin').withBundleAnalyzer({
-    enabled: process.env.ANALYZE === 'true',
-  })
-
-
-  return withSitemap({
-    sitemap: {
-      path: 'public/sitemap.xml',
-      generate: true,
-      siteUrl: 'https://www.bluepoint.ovh',
-    },
-    ...defaultConfig,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "drive.google.com"
-        },
-      ],
-    },
-  });
-};
-
+module.exports = withSitemap({
+  sitemap: {
+    path: 'public/sitemap.xml',
+    generate: true,
+    siteUrl: 'https://www.bluepoint.ovh',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com"
+      },
+    ],
+  },
+});
