c9547be9fc33c3384e7a330bb1ea03132f75f41f
unknown <laurent.supplisson@gmail.com>
2024-05-22 11:00:02 +0200

    addPictureAccueil

--- src/app/components/Accueil/ContenerCarouselAccueil/index.tsx
+++ src/app/components/Accueil/ContenerCarouselAccueil/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { CarouselAccueil } from "../carouselAccueil";
 
 const images = [
-    "https://drive.google.com/uc?id=1eBtn37Hwid4rfkGYjGJyg7jcvXoWL_fx",
+    "https://drive.google.com/uc?id=1_DDoJ_JH4FTR1OwpvRXvoZ9I3DGzvKp0",
     "https://drive.google.com/uc?id=1by3PzxpMqAotJgW-aWCHoNv8wo4dB1u1",
     "https://drive.google.com/uc?id=1C0z-uTOu5SZbr7U61tg1iqwIadkagtcf",
     "https://drive.google.com/uc?id=1MH5Bp2P92v1e7DsBpKvjIV4LnPrn6PYv",
