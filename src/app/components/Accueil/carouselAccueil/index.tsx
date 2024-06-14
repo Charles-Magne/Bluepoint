e9673e1b988ac115ceaecdd775745fab5f590435
unknown <laurent.supplisson@gmail.com>
2024-06-14 15:23:04 +0200

    changingPriorityCarouselBessan

--- src/app/components/Accueil/carouselAccueil/index.tsx
+++ src/app/components/Accueil/carouselAccueil/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import Slider from 'react-slick';
-import Image from 'next/image';
-import './style.scss';
-import { dot } from 'node:test/reporters';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
-
+"use client";
+import React from "react";
+import Slider from "react-slick";
+import Image from "next/image";
+import "./style.scss";
+import { dot } from "node:test/reporters";
+//import 'slick-carousel/slick/slick.css';
+//import 'slick-carousel/slick/slick-theme.css';
 
 interface CarouselProps {
   images: string[];
@@ -23,27 +22,30 @@ export const CarouselAccueil: React.FC<CarouselProps> = ({ images }) => {
     //autoplaySpeed: 8000,
     autoplaySpeed: 0,
     initialSlide: 0,
-    cssEase: 'linear',
+    cssEase: "linear",
     //dotsClass: 'carouselBessanBouton',
     appendDots: (dots: any) => (
-        <div className="carouselAccueilWrapper">
-          <ul className='carouselAccueilBouton'>{dots}</ul>
-        </div>
-      ),
+      <div className="carouselAccueilWrapper">
+        <ul className="carouselAccueilBouton">{dots}</ul>
+      </div>
+    ),
   };
 
   return (
-
-    <Slider className='carouselAccueilMain' {...settings}>
+    <Slider className="carouselAccueilMain" {...settings}>
       {images.map((image, index) => (
-        <div  key={index}>
-            <div className='carouselAccueilWrapper'>
-         <Image className='carouselleAccueilImage'priority={true} src={image} alt={`Slide ${index}`}  width={0}
-  height={0}
-  sizes="100vw"
-
-      />
-  </div>
+        <div key={index}>
+          <div className="carouselAccueilWrapper">
+            <Image
+              className="carouselleAccueilImage"
+              priority={false}
+              src={image}
+              alt={`Slide ${index}`}
+              width={0}
+              height={0}
+              sizes="100vw"
+            />
+          </div>
         </div>
       ))}
     </Slider>
