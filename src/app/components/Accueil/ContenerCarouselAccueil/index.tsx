f6b13d4f1c6b2617e3623291b8f6aca99a612292
unknown <laurent.supplisson@gmail.com>
2024-06-26 09:53:20 +0200

    addPictureInSite

--- src/app/components/Accueil/ContenerCarouselAccueil/index.tsx
+++ src/app/components/Accueil/ContenerCarouselAccueil/index.tsx
@@ -3,21 +3,19 @@ import React, { useEffect, useRef } from "react";
 import "./styles.scss";
 import Image from "next/image";
 import { CarouselAccueil } from "../carouselAccueil";
+import sushi from "./SiteSushi-Tiny (1).webp";
+import voyage from "./Capture d’écran 2024-05-22 105134 Tiny.png";
+import Chaussure from "./Capture d’écran 2024-06-03 111346 Tiny.png";
+import location from "./Capture-d’écran-2024-04-04-154324-Tiny (1).webp";
 
-const images = [
+/*const images = [
   "https://drive.google.com/uc?id=1bgI8V7wQW__AvOh9NBDjMTf_j1pHrDuy", //webp
  "https://drive.google.com/uc?id=1i17i-gYupkPgfl8NuYTxuveywyrzU8AO", //webp
  "https://drive.google.com/uc?id=1TJazaWb7I49A9l9_mNmr2D1csv0b3VlD", //webp
  "https://drive.google.com/uc?id=1IPFYdlFt30Wa7RU_rY3XKeZ-mBa8FNzY", //webp
-];
+];*/
 
-const imagesCdl = [
-  "https://res.cloudinary.com/dfrslvr04/image/upload/v1718352336/SiteSushi-Tiny_gvcgfp.webp", //Tiny
-  "https://res.cloudinary.com/dfrslvr04/image/upload/v1718352335/Capture-d_%C3%A9cran-2024-05-22-105134-Tiny_tdvt5h.webp", //Tiny
-  "https://res.cloudinary.com/dfrslvr04/image/upload/v1718352334/Capture-d_%C3%A9cran-2024-06-03-111346_kriru1.webp",//Tiny
-  "https://res.cloudinary.com/dfrslvr04/image/upload/v1718352334/Capture-d_%C3%A9cran-2024-04-04-154324-Tiny_vegxun.webp", //Tiny
-];
-
+const images = [sushi, voyage, Chaussure, location].map ((image) => image.src);
  const ContenerAccueil: React.FC = () => {
   return (
     <div>
--- src/app/components/contenerBessan/index.tsx
+++ src/app/components/contenerBessan/index.tsx
@@ -3,19 +3,23 @@ import React, { useEffect, useRef } from "react";
 import "./styles.scss";
 import Image from "next/image";
 import { CarouselBessan } from "../carouselBessan";
+import mairie from "./Tiny-Mairie_de_bessan_34550 (1).jpg";
+import vue from "./Tiny-bessan-retaillee_PLD4695_22x8.jpg";
+import marché from "./Tiny-marche-bessan-1200x675.jpg";
+import fleuve from "./Tiny-ouverture-bessan-770x430.jpg";
+import ane from ".//tiny-ane-de-bessan-5-960x640.jpg";
 
-const images = [
+/*const images = [
   "https://drive.google.com/uc?id=17FhQJl7J_cRHsj3360zS4An6UDTcZ_bf", //Tiny
   "https://drive.google.com/uc?id=198sDqVoL0bRwKFip7c-8jxppVZuOGDod",//Tiny
   "https://drive.google.com/uc?id=1iVHfNLejN8HaIDjrCO7JQ06xO6m9Izf3",//Tiny
   "https://drive.google.com/uc?id=179stj3O2pWgtV0EGd9dVPf2JPyjmf2mL", //Tiny
   "https://drive.google.com/uc?id=1AS5CeJ4XwsDTRsZNgp8CwUp6zRjFGlji", //Tiny
-];
+];*/
 
+const images = [mairie, vue, marché, fleuve, ane].map((image) => image.src);
 
-
-
- const ContenerBessan: React.FC = () => {
+const ContenerBessan: React.FC = () => {
   return (
     <div>
       {" "}
