834feb431b5575361a5fe71154fa0590d37ef2e8
unknown <laurent.supplisson@gmail.com>
2024-06-26 10:42:14 +0200

    chekingsitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,15 @@
+// L'URL de votre site
+const URL = "https://www.bluepoint.ovh"
+
+// Vos routes statiques
+const SITE_ROUTES = ["/", "/bluepoint", "/services", "/contact", "/plandusite", "/donneesdusite" ]
+
+export default async function sitemap() {
+
+  const routes = SITE_ROUTES.map(route => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }))
+
+  return [...routes, ...posts]
+}
